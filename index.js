e00bad0e45629832ae52cad2a2b382efde4faa8a
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-11-24 15:39:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    const preCats = [name, ...cats];
+    return preCats;
+}
+
+function removeLastCat(name) {
+    return cats.slice(0, cats.length - 1);
+}
+
+console.log(removeFirstCat())
+
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
